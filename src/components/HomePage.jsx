3de23f4b58a708f2f80a41ff54f1c35328d218e1
Emre-Karatekeli <emrekaratekeli89@gmail.com>
2024-11-06 17:35:35 +0300

    homepage başlar

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import './HomePage.css';
+
+export default function HomePage () {
+    return (
+        <div className='homepage'>
+            <h2>Teknolojik Yemekler</h2>
+            <h1>KOD ACIKTIRIR<br/>PiZZA, DOYURUR</h1>
+            <button>ACIKTIM</button>
+            <img src="https://s3-alpha-sig.figma.com/img/2b78/c96a/4a4acb450e3bbb3750c05c017efa1f61?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NikPWQHZOB3RrS3~TRMNrbDoh92~B3UvlHTe-70JR5zHT9D0Pj5-cRrzHRrPhNikAsFz938XNnkPFUJODWRMwlJ0MIvVjQNU59xGfzI8GkuqyU0NHHSkmYwMiXZoUM8GdUMQccwfmu2BE8Ww6ztcZh~qtfA07lxt2PBDbk06jiPepElIemZWy~2HUZXpZHRCPO-Q2aD-3ZmVuhRO1HoovQ9Q43vCLCjufB3IjGq76e0WnsYPKKb5LvMCyxCets9FjHd1ITudwH47MV5mf~4waU824zLLlet5Fd921qLWVUJDQwxiFx4J65x3O4lDw5RtH2vj9Wi7jJxINyLqbA9Yyw__" alt="" />
+
+        /</div>
+    )
+}
